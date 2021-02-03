843e6e5def4297d2994aae450869a9c0d6042570
Billy Joe Ramos <billyjoersantos@outlook.com>
2021-02-03 16:50:45 -0300

    Update login.page.ts

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -29,27 +29,3 @@ export class LoginPage implements OnInit {
   }
 
 }
-
-// <!-- The core Firebase JS SDK is always required and must be listed first -->
-// <script src="https://www.gstatic.com/firebasejs/8.2.5/firebase-app.js"></script>
-
-// <!-- TODO: Add SDKs for Firebase products that you want to use
-//      https://firebase.google.com/docs/web/setup#available-libraries -->
-// <script src="https://www.gstatic.com/firebasejs/8.2.5/firebase-analytics.js"></script>
-
-// <script>
-//   // Your web app's Firebase configuration
-//   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
-//   var firebaseConfig = {
-//     apiKey: "AIzaSyBocg_h6lQeloWzJMnuHFq2OeS45LKoE-w",
-//     authDomain: "old-skool-basketball.firebaseapp.com",
-//     projectId: "old-skool-basketball",
-//     storageBucket: "old-skool-basketball.appspot.com",
-//     messagingSenderId: "432824905625",
-//     appId: "1:432824905625:web:9a34af8eba44cc020a6d08",
-//     measurementId: "G-VHC3CX7J8T"
-//   };
-//   // Initialize Firebase
-//   firebase.initializeApp(firebaseConfig);
-//   firebase.analytics();
-// </script>
